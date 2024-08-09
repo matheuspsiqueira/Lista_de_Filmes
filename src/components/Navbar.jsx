9c1f6e26d82609ce35e5ed2bca2b610bb96a1259
Matheus <matheuspontessiqueira@gmail.com>
2024-08-09 13:17:58 -0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <nav id='navbar'>
         <h2>
             <Link to='/'>
-            <BiCameraMovie /> MoviesLib 
+            <img src="/src/pages/rolo-film.png" alt="" /> MoviesLib 
             </Link>
         </h2>
           <form onSubmit={handleSubmit}>
